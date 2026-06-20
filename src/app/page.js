bd265cace3238c84871e6d98972a9da8253d7bf3
Pranto408 <prantodatta408@gmail.com>
2026-06-20 23:24:07 +0600

    TopLocations Section Created

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/homepage/Banner";
+import TopLocations from "@/components/homepage/TopLocations";
 import WhyChooseUs from "@/components/homepage/WhyChooseUs";
 
 
 export default function Home() {
   return <div>
     <Banner />
-    <WhyChooseUs/>
+    <WhyChooseUs />
+    <TopLocations/>
   </div>;
 }
